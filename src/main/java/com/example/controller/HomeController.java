e0ca1c11df8d18c565a8cbb62e3d34d77db10b82
ikuno <taro.tanaka@example.com>
2026-04-28 11:49:27 +0900

    トップページと商品一覧ページを表示するController・テンプレートを追加

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -2,15 +2,16 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	@ResponseBody
+
 	public String home() {
-		return "Hello Spring";
+		//		return "Hello Spring";
+		// @ResponseBpdyがなくなったらtemplates/home.html を表示する
+		return "home";
 	}
 
 }
