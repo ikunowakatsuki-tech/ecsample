ca0f22fac58bd00d9f9d2311101deda46a1904ef
ikuno <taro.tanaka@example.com>
2026-05-08 10:36:55 +0900

    フォーム入力値のバリデーションと相関チェックを@Validatedで実装

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,36 +1,39 @@
 package com.example.controller;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.ecsample.form.UserForm;
 
 @Controller
 public class UserController {
 
-  @GetMapping("/register")
-  public String showForm() {
-  return "user/register";
-  }
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		model.addAttribute("form", new UserForm());
+		return "user/register";
+	}
 
-  @PostMapping("/register")
-  public String submitForm(
-  @RequestParam("name") String name,
-  @RequestParam("email") String email,
-  @RequestParam("password") String password,
-  Model model) {
+	@PostMapping("/register")
+	public String submitForm(
+			@Validated @ModelAttribute("form") UserForm form,
+			BindingResult bindingResult,
+			Model model) {
 
-  // name属性に対応した値が受け取れることを確認
-  System.out.println("name = " + name);
-  System.out.println("email = " + email);
-  System.out.println("password = " + password);
+		if (bindingResult.hasErrors()) {
+			return "user/register"; // エラー時はフォームに戻す
+		}
+		// まとめて受け取れているか確認
+		System.out.println("name = " + form.getName());
+		System.out.println("email = " + form.getEmail());
+		System.out.println("password = " + form.getPassword());
 
-  model.addAttribute("name", name);
-  model.addAttribute("email", email);
-  model.addAttribute("password", password);
-  return "user/result";
-  }
+		model.addAttribute("form", form);
+		return "user/result";
+	}
 }
